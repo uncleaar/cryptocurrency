442f1420d1469e11ff10f79ac25a40aad1066341
harld-x <danilyudin723@gmail.com>
2021-09-08 15:19:46 +0700

    detailpage

--- src/pages/CoinDetailPage.jsx
+++ src/pages/CoinDetailPage.jsx
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
+import CoinData from '../components/CoinData';
+import HistoryChart from '../components/HistoryChart';
+import coinGecko from '../apis/coinGecko';
 
 const CoinDetailPage = () => {
   const { id } = useParams();
 
-  return <div></div>;
+  const [coinData, setCoinData] = useState({});
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      const [day, week, year, detail] = await Promise.all([
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: 'usd',
+            days: '1',
+          },
+        }),
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: 'usd',
+            days: '7',
+          },
+        }),
+        coinGecko.get(`/coins/${id}/market_chart/`, {
+          params: {
+            vs_currency: 'usd',
+            days: '365',
+          },
+        }),
+        coinGecko.get('/coins/markets/', {
+          params: {
+            vs_currency: 'usd',
+            ids: id,
+          },
+        }),
+      ]);
+
+      setCoinData({
+        day: day.data.prices,
+        week: week.data.prices,
+        year: year.data.prices,
+        detail: detail.data,
+      });
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  const renderData = () => {
+    if (isLoading) {
+      return <div>Loading</div>;
+    }
+    return (
+      <div className="coinlist">
+        <HistoryChart />
+        <CoinData />
+      </div>
+    );
+  };
+
+  return renderData();
 };
 
 export default CoinDetailPage;
